0576e04ee9ac0a4a4c9ff08f75633ad79470388b
Eric Peterson <ericpeterson@spotify.com>
2021-05-25 16:29:15 +0200

    Add unit test for quoted cwd
    Signed-off-by: Eric Peterson <ericpeterson@spotify.com>

---
+++ packages/techdocs-cli/src/lib/mkdocsServer.test.ts
@@ -0,0 +1,55 @@
+import { runMkdocsServer } from './mkdocsServer';
+import { run } from './run';
+
+jest.mock('./run', () => {
+  return {
+    run: jest.fn(),
+  };
+});
+
+describe('runMkdocsServer', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  describe('docker', () => {
+    it('should run docker directly by default', async () => {
+      await runMkdocsServer({});
+
+      const quotedCwd = `'${process.cwd()}':/content`;
+      expect(run).toHaveBeenCalledWith('docker', expect.arrayContaining([
+        'run',
+        quotedCwd,
+        '8000:8000',
+        'serve',
+        '--dev-addr',
+        '0.0.0.0:8000',
+      ]), expect.objectContaining({}));
+    });
+
+    it('should accept port option', async () => {
+      await runMkdocsServer({ port: '5678'});
+      expect(run).toHaveBeenCalledWith('docker', expect.arrayContaining([
+        '5678:5678',
+        '0.0.0.0:5678',
+      ]), expect.objectContaining({}));
+    });
+  });
+
+  describe('mkdocs', () => {
+    it('should run mkdocs if specified ', async () => {
+      await runMkdocsServer({ useDocker: false });
+
+      expect(run).toHaveBeenCalledWith('mkdocs', expect.arrayContaining([
+        'serve', '--dev-addr', '127.0.0.1:8000',
+      ]), expect.objectContaining({}));
+    });
+
+    it('should accept port option', async () => {
+      await runMkdocsServer({ useDocker: false, port: '5678'});
+      expect(run).toHaveBeenCalledWith('mkdocs', expect.arrayContaining([
+        '127.0.0.1:5678',
+      ]), expect.objectContaining({}));
+    });
+  });
+});
