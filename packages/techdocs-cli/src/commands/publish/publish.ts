94cfdee30a013e80c868f05d35f5e590aa5de022
Eric Peterson <ericpeterson@spotify.com>
2021-07-16 17:21:10 +0200

    Refactor Publisher config into a helper.
    Co-authored-by: Ruben Lindström <rubenl@spotify.com>
    Signed-off-by: Eric Peterson <ericpeterson@spotify.com>

--- packages/techdocs-cli/src/commands/publish/publish.ts
+++ packages/techdocs-cli/src/commands/publish/publish.ts
@@ -16,107 +16,16 @@
 import { resolve } from "path";
 import { Command } from "commander";
 import { createLogger } from "../../lib/utility";
-import { ConfigReader } from "@backstage/config";
 import { SingleHostDiscovery } from "@backstage/backend-common";
 import { Publisher } from "@backstage/techdocs-common";
 import { Entity } from "@backstage/catalog-model";
+import { getValidPublisherConfig } from "../helpers"
+
 
 export default async function publish(cmd: Command) {
   const logger = createLogger({ verbose: cmd.verbose });
 
-  // Assuming that proper credentials are set in Environment variables
-  // for the respective GCS/AWS clients to work.
-
-  if (
-    !["awsS3", "googleGcs", "azureBlobStorage", "openStackSwift"].includes(
-      cmd.publisherType
-    )
-  ) {
-    logger.error(`Unknown publisher type ${cmd.publisherType}`);
-    throw new Error();
-  }
-
-  let publisherConfig;
-  if ("azureBlobStorage" === cmd.publisherType) {
-    if (!cmd.azureAccountName) {
-      logger.error(
-        `azureBlobStorage requires --azureAccountName to be specified`
-      );
-      throw new Error();
-    }
-    publisherConfig = {
-      type: cmd.publisherType,
-      [cmd.publisherType]: {
-        containerName: cmd.storageName,
-        credentials: {
-          accountName: cmd.azureAccountName,
-          accountKey: cmd.azureAccountKey
-        }
-      }
-    };
-  } else if ("awsS3" === cmd.publisherType) {
-    publisherConfig = {
-      type: cmd.publisherType,
-      [cmd.publisherType]: {
-        bucketName: cmd.storageName,
-        ...(cmd.awsRoleArn && { credentials: { roleArn: cmd.awsRoleArn } }),
-        ...(cmd.awsEndpoint && { endpoint: cmd.awsEndpoint }),
-        ...(cmd.awsS3ForcePathStyle && { s3ForcePathStyle: true })
-      }
-    };
-  } else if ("openStackSwift" === cmd.publisherType) {
-    let ismissingVariable = false;
-
-    ["osUsername", "osPassword", "osAuthUrl", "osRegion"].forEach(
-      (param: string) => {
-        if (!cmd[param]) {
-          ismissingVariable = true;
-          logger.error(`openStackSwift requires --${param} to be specified`);
-        }
-      }
-    );
-
-    if (ismissingVariable) {
-      throw new Error();
-    }
-
-    publisherConfig = {
-      type: cmd.publisherType,
-      [cmd.publisherType]: {
-        containerName: cmd.storageName,
-        credentials: {
-          username: cmd.osUsername,
-          password: cmd.osPassword
-        },
-        authUrl: cmd.osAuthUrl,
-        region: cmd.osRegion,
-        keystoneAuthVersion: cmd.osAuthVersion,
-        domainId: cmd.osDomainId,
-        domainName: cmd.osDomainName
-      }
-    };
-  } else {
-    publisherConfig = {
-      type: cmd.publisherType,
-      [cmd.publisherType]: {
-        bucketName: cmd.storageName
-      }
-    };
-  }
-
-  const config = new ConfigReader({
-    // This backend config is not used at all. Just something needed a create a mock discovery instance.
-    backend: {
-      baseUrl: "http://localhost:7000",
-      listen: {
-        port: 7000
-      }
-    },
-    techdocs: {
-      publisher: publisherConfig
-    }
-  });
-
+  const config = getValidPublisherConfig(cmd);
   const discovery = SingleHostDiscovery.fromConfig(config);
   const publisher = await Publisher.fromConfig(config, { logger, discovery });
 
