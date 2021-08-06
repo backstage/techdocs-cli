3f176fb6110309d846cd81da4d2a6e1e7abdc091
Andrew Thauer <athauer@wealthsimple.com>
2021-08-05 22:15:54 -0400

    reorder e2e test expectations

--- packages/techdocs-cli/src/e2e.test.ts
+++ packages/techdocs-cli/src/e2e.test.ts
@@ -1,40 +1,34 @@
-import { spawn } from "child_process";
+import { spawn } from 'child_process';
 
-describe("end-to-end", () => {
-  it("shows help text", async () => {
-    const proc = await executeTechDocsCliCommand(["--help"]);
+describe('end-to-end', () => {
+  it('shows help text', async () => {
+    const proc = await executeTechDocsCliCommand(['--help']);
 
+    expect(proc.combinedStdOutErr).toContain('Usage: techdocs-cli [options]');
     expect(proc.exit).toEqual(0);
-    expect(proc.combinedStdOutErr).toContain("Usage: techdocs-cli [options]");
   });
 
-  it("can generate", async () => {
+  it('can generate', async () => {
     jest.setTimeout(10000);
-    const proc = await executeTechDocsCliCommand(["generate", "--no-docker"], {
-      cwd: "../../",
+    const proc = await executeTechDocsCliCommand(['generate', '--no-docker'], {
+      cwd: '../../',
       killAfter: 8000,
     });
 
-    // eslint-disable-next-line no-console
-    console.log(proc.combinedStdOutErr);
-
+    expect(proc.combinedStdOutErr).toContain('Successfully generated docs');
     expect(proc.exit).toEqual(0);
-    expect(proc.combinedStdOutErr).toContain("Successfully generated docs");
   });
 
-  it("can serve", async () => {
+  it('can serve', async () => {
     jest.setTimeout(10000);
-    const proc = await executeTechDocsCliCommand(["serve", "--no-docker"], {
-      cwd: "../../",
+    const proc = await executeTechDocsCliCommand(['serve', '--no-docker'], {
+      cwd: '../../',
       killAfter: 8000,
     });
 
-    // eslint-disable-next-line no-console
-    console.log(proc.combinedStdOutErr);
-
+    expect(proc.combinedStdOutErr).toContain('Starting mkdocs server');
+    expect(proc.combinedStdOutErr).toContain('Serving docs in Backstage at');
     expect(proc.exit).toEqual(0);
-    expect(proc.combinedStdOutErr).toContain("Starting mkdocs server");
-    expect(proc.combinedStdOutErr).toContain("Serving docs in Backstage at");
   });
 });
 
@@ -52,14 +46,14 @@ type ExecuteCommandOptions = {
 
 function executeTechDocsCliCommand(
   args: string[],
-  opts: ExecuteCommandOptions = {}
+  opts: ExecuteCommandOptions = {},
 ): Promise<CommandResponse> {
-  return new Promise((resolve) => {
+  return new Promise(resolve => {
     const pathToCli = `${__dirname}/../bin/techdocs-cli`;
     const commandResponse = {
-      stdout: "",
-      stderr: "",
-      combinedStdOutErr: "",
+      stdout: '',
+      stderr: '',
+      combinedStdOutErr: '',
       exit: 0,
     };
 
@@ -71,28 +65,28 @@ function executeTechDocsCliCommand(
     const stdErrChunks: any[] = [];
     const combinedChunks: any[] = [];
 
-    listen.stdout.on("data", (data) => {
+    listen.stdout.on('data', data => {
       stdOutChunks.push(data);
       combinedChunks.push(data);
     });
 
-    listen.stderr.on("data", (data) => {
+    listen.stderr.on('data', data => {
       stdErrChunks.push(data);
       combinedChunks.push(data);
     });
 
-    listen.on("exit", (code) => {
+    listen.on('exit', code => {
       commandResponse.exit = code as number;
-      commandResponse.stdout = Buffer.concat(stdOutChunks).toString("utf8");
-      commandResponse.stderr = Buffer.concat(stdErrChunks).toString("utf8");
+      commandResponse.stdout = Buffer.concat(stdOutChunks).toString('utf8');
+      commandResponse.stderr = Buffer.concat(stdErrChunks).toString('utf8');
       commandResponse.combinedStdOutErr =
-        Buffer.concat(combinedChunks).toString("utf8");
+        Buffer.concat(combinedChunks).toString('utf8');
       resolve(commandResponse);
     });
 
     if (opts.killAfter) {
       setTimeout(() => {
-        listen.kill("SIGTERM");
+        listen.kill('SIGTERM');
       }, opts.killAfter);
     }
   });
