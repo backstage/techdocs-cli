ec7ba4c303b6ffd11cc59d425733f36bd66b2edb
Camila Belo <camilaibs@gmail.com>
2021-10-21 20:10:46 +0200

    fix: search api impl
    Signed-off-by: Camila Belo <camilaibs@gmail.com>

--- packages/embedded-techdocs-app/src/apis.ts
+++ packages/embedded-techdocs-app/src/apis.ts
@@ -36,6 +36,7 @@ import {
   TechDocsStorageApi,
   techdocsStorageApiRef,
 } from '@backstage/plugin-techdocs';
+import { searchPlugin } from '@backstage/plugin-search';
 
 // TODO: Export type from plugin-techdocs and import this here
 // import { ParsedEntityId } from '@backstage/plugin-techdocs'
@@ -196,4 +197,5 @@ export const apis: AnyApiFactory[] = [
     deps: { configApi: configApiRef },
     factory: ({ configApi }) => ScmIntegrationsApi.fromConfig(configApi),
   }),
+  ...searchPlugin.getApis(),
 ];
